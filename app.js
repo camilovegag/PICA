c4c314ad2212c114e758b5bc304a0350322a336e
camilovegag <camilovegag@unisabana.edu.co>
2024-08-01 00:59:37 -0500

    Fix port at app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const userRoutes = require("./routes/userRoutes");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.DB_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
