81f6e441cbf59473bef9c4b264eaf8b86f2ef753
camilovegag <camilovegag@unisabana.edu.co>
2024-08-01 23:49:04 -0500

    Add logger to database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import logger from "../utils/logger";
 
 dotenv.config();
 
@@ -12,6 +13,14 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+});
+
+// Log pool creation
+logger.info("Database connection pool created with configuration", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 export default pool;
