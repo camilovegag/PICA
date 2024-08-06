09fb9f62585cfd84c150e4e876a59ecfb35bf4d9
camilovegag <camilovegag@unisabana.edu.co>
2024-08-05 20:29:58 -0500

    Create init.sql file

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    fecha DATE,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
