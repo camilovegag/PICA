edbd36e8a44aaa17eb6cc402558e5f42a6b96799
camilovegag <camilovegag@unisabana.edu.co>
2024-08-07 10:24:26 -0500

    Add new put router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,14 @@ import {
   createUser,
   getUsers,
   getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/", createUser);
 router.get("/", getUsers);
 router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
 
 export default router;
